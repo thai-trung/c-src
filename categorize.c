703c90c58407b11efb9b3fbe8613e9201ce608f5
thai-trung <thaitrung1983@gmail.com>
2020-03-17 14:01:55 +0700

    add source categorize.c again

--- categorize.c
+++ categorize.c
@@ -1,25 +1,24 @@
-/*
-demonstration arguments command line
-*/
+//implement file stream
 #include <stdio.h>
 #include <string.h>
 
-int main(int agrc, char *argv[]){
-
-    if (agrc != 3){
-       fprintf(stderr, "You need to give 3 arguments!\n") ;
-       return 1;
+int main(int argc, char *argv[]){
+    if (argc != 3)
+    {
+        fprintf(stderr, "You need give 3 arguments!\n");
+        return 1;
     }
-    
+
     FILE *in;
-    if (!(in = fopen("spooky.csv", "r"))){
-        fprintf(stderr, "Cannot find the file.\n");
+    if (!(in = fopen("spooky.csv", "r")))
+    {
+        fprintf(stderr, "Cannot find the file!\n");
         return 1;
     }
 
     FILE *out = fopen(argv[2], "w");
     char line[80];
-    while (fscanf(in, "%79[^\n]\n", line)== 1)
+    while (fscanf(in, "%79[^\n]\n", line) == 1)
     {
         if (strstr(line, argv[1]))
         {
@@ -27,5 +26,6 @@ int main(int agrc, char *argv[]){
         }
         
     }
-    return 0;
+
+    return 0; 
 }
