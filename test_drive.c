e6d2a23c2e1e944366f9a795f6c3d8de040b6de7
thai-trung <thaitrung1983@gmail.com>
2020-03-17 13:15:30 +0700

    add source test_drive.c again

--- test_drive.c
+++ test_drive.c
@@ -1,26 +1,23 @@
-/*
-demonstration Pointer function
-*/
+//implement sorting function in C
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare_scores(const void *score_a, const void *score_b){
+int compare_score(const void *score_a, const void *score_b){
     int a = * (int *)score_a;
     int b = * (int *)score_b;
-
     return a-b;
 }
 
-int main(void){
-    int scores[] = {65, 71, 13, 56, 198, 14, 84};
-    qsort (scores, 7, sizeof(int), compare_scores);
-    puts("Order Scores:");
+int main(void)
+{
+    int scores[] = {131, 19, 157, 84, 172, 144, 96};
+    qsort(scores, 7, sizeof(int), compare_score);
+    puts("Order scores:");
     for (int i = 0; i < 7; i++)
     {
         printf("%i\t", scores[i]);
     }
-
     puts("");
     
     return 0;
